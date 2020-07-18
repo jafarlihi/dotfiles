be34141fbabd0c04433834524a57f61483cf3d4c
Hikmat Jafarli <jafarlihi@gmail.com>
2020-07-18 17:49:42 +0400

    Fix sojobs

--- scripts/bin/sojobs.py
+++ scripts/bin/sojobs.py
@@ -6,11 +6,12 @@ feed = feedparser.parse('https://stackoverflow.com/jobs/feed')
 
 d = {}
 for entry in feed.entries:
-    for tag in entry.tags:
-        if tag.term in d:
-            d[tag.term] += 1
-        else:
-            d[tag.term] = 1
+    if hasattr(entry, 'tags'):
+        for tag in entry.tags:
+            if tag.term in d:
+                d[tag.term] += 1
+            else:
+                d[tag.term] = 1
 
 for key, value in sorted(d.items(), key=lambda item: item[1]):
     print(key + ": " + str(value))
