d97f04b0262dc167a0fad0de3346bdc566943783
Hikmat Jafarli <jafarlihi@gmail.com>
2024-01-04 21:26:31 +0100

    Change qutebrowser default search engine

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -1,4 +1,7 @@
 c.fonts.default_size = "20px"
 c.zoom.default = "150%"
 c.colors.webpage.darkmode.enabled = True
+c.url.searchengines = {
+    'DEFAULT':  'https://google.com/search?hl=en&q={}',
+}
 config.load_autoconfig()
