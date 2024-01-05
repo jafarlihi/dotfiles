4399331636629765e76639f415c1d4f1b3a33252
Hikmat Jafarli <jafarlihi@gmail.com>
2024-01-05 04:22:24 +0100

    Make neo-tree close on file selection for LazyVim

---
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -0,0 +1,14 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- Add any additional autocmds here
+
+require("neo-tree").setup({
+  event_handlers = {
+    {
+      event = "file_opened",
+      handler = function(file_path)
+        require("neo-tree.command").execute({ action = "close" })
+      end,
+    },
+  },
+})
